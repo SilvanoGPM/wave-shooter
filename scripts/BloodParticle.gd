97341e1c5944f2ff60f7c5551e3c39de213c422d
SkyG0D <silvanosilvino@hotmail.com>
2022-07-30 19:20:27 -0300

    feat: add clear for blood particles

--- scripts/BloodParticle.gd
+++ scripts/BloodParticle.gd
@@ -1,4 +1,13 @@
 extends CPUParticles2D
 
+var CLEAR_PARTICLE_OPACITY_PERCENT = 0.05
+
 func _on_timer_timeout():
 	Global.stop_node(self)
+
+func _on_clear_timer_timeout():
+	modulate.a = lerp(modulate.a, 0, 0.1)
+	
+
+	if modulate.a <= CLEAR_PARTICLE_OPACITY_PERCENT:
+		queue_free()
